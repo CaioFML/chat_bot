d14bc2b3d129ffb18c89ce2f5827ea17de01c907
CaioFML <caio.felipemotta@hotmail.com>
2020-02-04 19:23:34 -0300

    database

--- config/database.rb
+++ config/database.rb
@@ -19,3 +19,16 @@ configure :development do
     host: 'postgres'
   }
 end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+
+  set :database, {
+    adapter:  'postgresql',
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  }
+end
